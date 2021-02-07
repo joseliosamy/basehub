7f6801011debfc75f708e00d7d18620d9c1b0b17
Josélio Samy <joseliobackup@gmail.com>
2021-02-07 04:47:10 -0300

    slider added

---
+++ scripts/carousel.js
@@ -0,0 +1,23 @@
+const carouselSlide = document.querySelector('.slide-images');
+const carouselImages = document.querySelectorAll('.slide-images img');
+
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click', ()=>{
+    if(counter >= carouselImages.length -1) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+prevBtn.addEventListener('click', ()=>{
+    if(counter <= 0) return;
+    console.log(counter)
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
