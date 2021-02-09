0cd16de422ddeb998b0e01e4c46dd50864f6aaf7
Josélio Samy <joseliobackup@gmail.com>
2021-02-09 01:38:00 -0300

    projects page added

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,18 +1,19 @@
 const carouselSlide = document.querySelector('.slide-images');
-const carouselImages = document.querySelectorAll('.slide-images img');
+const carouselImages = document.querySelectorAll('.slide-images .box-img');
+const boxImg = document.querySelectorAll('.box-img');
 
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
 
 let counter = 0;
 const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
+// carousel
 nextBtn.addEventListener('click', ()=>{
     if(counter >= carouselImages.length -1) return;
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = 'translateX(' + ((-size * counter) + 50) + 'px)';
 })
 prevBtn.addEventListener('click', ()=>{
     if(counter <= 0) return;
@@ -20,4 +21,24 @@ prevBtn.addEventListener('click', ()=>{
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+// open img
+
+carouselImages.forEach(carouselImages=>{
+    carouselImages.addEventListener('click',()=>{
+        if(carouselImages.getAttribute('class') == "original"){
+            carouselImages.classList.remove('original');
+            carouselImages.classList.add('modal');
+        }else{
+            carouselImages.classList.remove('modal');
+            carouselImages.classList.add('original');
+        }
+    })
+    carouselImages.addEventListener(e=>{
+        if(e.target.classList.contains('modal')){
+            carouselImages.classList.remove('modal');
+            carouselImages.classList.add('original');
+        }
+    })
 })
